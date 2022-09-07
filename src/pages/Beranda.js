731c8d34941a3fa748428a881bcf353013fd2d7d
Yanzen <yanwarsolah@gmail.com>
2022-09-07 19:26:00 +0700

    navigator - redirect

--- src/pages/Beranda.js
+++ src/pages/Beranda.js
@@ -1,10 +1,24 @@
-
+import { useSearchParams, useNavigate } from "react-router-dom"
 
 function Beranda() {
-  
+  const [qParams, setQParams] = useSearchParams()
+  const navigator = useNavigate();
 
+  const redirect = (e) => {
+    navigator(`/about/${qParams.get('nama')}/${qParams.get('umur')}`)
+  }
+
+  let nama = qParams.get("nama")
   return <>
-    <h1>Selamat datang di website saya!</h1>
+    <h1
+      onClick={redirect}
+    >Selamat datang di website saya!</h1>
+    <p>Nama: {qParams.get("nama")}</p>
+    <p
+      onClick={(e) => setQParams({umur: 19, nama: nama})}
+    >Umur: {qParams.get("umur")}</p>
+
+
   </>
 }
 
